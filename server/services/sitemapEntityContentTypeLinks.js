da191845865dbe7acfd36547880c820a2e10dcee
Aditya Naik <anaik@zam.com>
2023-10-04 11:21:09 -0700

    adds db lookup to find which sitemap file contains entity

--- server/services/sitemapEntityContentTypeLinks.js
+++ server/services/sitemapEntityContentTypeLinks.js
@@ -1,5 +1,39 @@
 const { factories } = require('@strapi/strapi');
 
+const uid = 'plugin::sitemap.sitemap-entity-content-type-link';
 module.exports = factories.createCoreService(
-  'plugin::sitemap.sitemap-entity-content-type-link',
+  uid,
+  ({ strapi }) => ({
+    async addOrUpdateMany(links) {
+      const data = [];
+      const select = '*';
+      for (let x = 0; x < links.length; x += 1) {
+        const link = links[x];
+        const { entityId, contentType, chunkName } = link;
+        const where = {
+          entityId,
+          contentType,
+        };
+        const entity = await strapi.db.query(uid).findOne({
+          select,
+          where,
+        });
+        if (!entity) data.push(link);
+        else {
+          const { id, chunkName: entityChunkName } = entity;
+          if (entityChunkName !== chunkName) {
+            await strapi.db.query(uid).update({
+              where: { id },
+              data: {
+                chunkName,
+              },
+            });
+          }
+        }
+      }
+      if (data.length) {
+        await strapi.db.query(uid).createMany({ data });
+      }
+    },
+  }),
 );
